b292baae82e934dcfb2423299c2b5632abc11cac
Михаил Аликбаров <michael.alikbarov@yandex.ru>
2022-09-28 23:36:31 +0500

    устранены замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,34 @@
-const popupOpenButton = document.querySelector(".profile__open-popup");
 const popup = document.querySelector(".popup");
+const popupOpenButton = document.querySelector(".profile__open-popup");
 const popupCloseButton = popup.querySelector(".popup__close");
-const popupSaveButton = popup.querySelector(".popup__save")
+const popupSaveButton = popup.querySelector(".popup__save");
 
-const popupToggle = function (event) {
-  popup.classList.toggle("popup_opened");
-  console.log("нажали на кнопку");
-};
-
-popupOpenButton.addEventListener("click", popupToggle);
-popupCloseButton.addEventListener("click", popupToggle);
-popupSaveButton.addEventListener("click", popupToggle);
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
 
 let formElement = document.querySelector(".popup");
-let nameInput = formElement.querySelector(".popup__name");
-let jobInput = formElement.querySelector(".popup__info");
+let nameInput = formElement.querySelector(".popup__info_name");
+let jobInput = formElement.querySelector(".popup__info_job");
+
+function formOpenHandler () {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  console.log(nameInput.value);
+};
+
+const popupToggle = function (event) {
+    popup.classList.toggle("popup_opened");
+};
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let name = nameInput.value;
-    let job = jobInput.value;
-    let profileTitle = document.querySelector(".profile__title");
-    let profileSubtitle = document.querySelector(".profile__subtitle");
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+};
 
-    profileTitle.textContent = name;
-    profilesubTitle.textContent = job;
-}
+popupOpenButton.addEventListener("click", popupToggle, formOpenHandler());
+popupCloseButton.addEventListener("click", popupToggle);
+popupSaveButton.addEventListener("click", popupToggle);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
