bceca3c049a0e1264579fa3e25a4a1d636c12933
Михаил Аликбаров <michael.alikbarov@yandex.ru>
2022-09-29 18:18:43 +0500

    поправлено заполнение и сохранение формы

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,21 @@ const popupOpenButton = document.querySelector(".profile__open-popup");
 const popupCloseButton = popup.querySelector(".popup__close");
 const popupSaveButton = popup.querySelector(".popup__save");
 
-let profileTitle = document.querySelector(".profile__title");
-let profileSubtitle = document.querySelector(".profile__subtitle");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
-let formElement = document.querySelector(".popup");
-let nameInput = formElement.querySelector(".popup__info_name");
-let jobInput = formElement.querySelector(".popup__info_job");
+const formElement = document.querySelector(".popup__input-info");
+const nameInput = popup.querySelector(".popup__info_name");
+const jobInput = popup.querySelector(".popup__info_job");
 
-function formOpenHandler () {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-  console.log(nameInput.value);
+function popupOpen () {
+    popup.classList.add("popup_opened");
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 };
 
-const popupToggle = function (event) {
-    popup.classList.toggle("popup_opened");
+function popupClose () {
+  popup.classList.remove("popup_opened");
 };
 
 function formSubmitHandler (evt) {
@@ -26,9 +26,9 @@ function formSubmitHandler (evt) {
     profileSubtitle.textContent = jobInput.value;
 };
 
-popupOpenButton.addEventListener("click", popupToggle, formOpenHandler());
-popupCloseButton.addEventListener("click", popupToggle);
-popupSaveButton.addEventListener("click", popupToggle);
+popupOpenButton.addEventListener("click", popupOpen);
+popupCloseButton.addEventListener("click", popupClose);
+popupSaveButton.addEventListener("click", popupClose);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
